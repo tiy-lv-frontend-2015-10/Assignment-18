e547237befd4aa8d73109c6586fbd97f0d2647c7
noemiostorga <noemiostorga@gmail.com>
2015-11-11 14:13:21 -0800

    done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-$.ajaxSetup({
-  headers: {
-    'X-Parse-Application-Id': 'HDSKUFLKzJWxz1aLcLqNc8jdnE2Twt61JLlwkKMf',
-    'X-Parse-REST-API-Key': 'muxdDbLUqj2HSYbDZLPV1puTAwXfJnCdu1wa2J3n'
-  }
-});
 
 ///////////////////////////////
 
@@ -13,7 +7,6 @@ var Pets = Backbone.Model.extend({
   },
   defaults: {
     name: null,
-    alive: null,
     type: null,
     age: null,
   },
@@ -21,17 +14,11 @@ var Pets = Backbone.Model.extend({
     if (!attrs.name) {
       return "Name is required";
     }
-    if(!attrs.alive){
-      return "alive is required"
-    }
     if(!attrs.type){
       return "type must be required"
     }
     if (typeof attrs.name !== 'string') {
       return "Name is must be a string";
-    }
-    if(typeof attrs.alive !== 'string'){
-      return "alive is required"
     }
     if (typeof attrs.type !== 'string') {
       return "type must a string";
@@ -40,46 +27,87 @@ var Pets = Backbone.Model.extend({
       return "age is required";
     }
     if (isNaN(Number(attrs.age))) {
-      return "Published Year should be a number";
+      return "age should be a number";
     }
 
   },
-  _parse_class_name: "Pets"
+  _parse_class_name: "Pets",
+  idAttribute: "objectId",
 });
 
-var pets = new Pets({
-    name: "Cisco",
-    age: 7,
-    alive: true,
-    type: "dog"
-})
+var Router = Backbone.Router.extend({
+  initialize: function () {
+    Backbone.history.start({pushState: true});
+  },
+  routes: {
+    "pets": "pets",
+    " ":"index"
+  }
+});
+
+ var PetsCollection = new Pets();
 
 var Pets = Backbone.Collection.extend({
   model: Pets,
   _parse_class_name: "Pets"
 });
 
-var PetsCollection = new Pets();
+var pets = new Pets({
+    name: "Cisco",
+    type: "dog",
+    age: 7
+})
 
-pets.save (null, {
+
+
+/*pets.save(null, {
   success: function(resp) {
     console.log(resp)
 
     PetsCollection.fetch({
       success: function(resp) {
         console.log("success: ", resp);
-      }, error: function (err) {
+      }, 
+      error: function (err) {
         console.log("error: ", err);
       }
+
     })
   },
+
   error: function (err) {
     console.log(err)
   }
+});
 
+*/
+
+
+
+var router = new Router();
+
+router.on ('route:pets', function (objectId) {
+  var pets = new Pets({objectId:objectId});
+  pets.fetch();
+  console.log(pets);
+});
+
+router.on('route:index', function(){
+  console.log("home page");
+});
+
+router.on('route:pets', function() {
+  console.log("pets pages");
 });
 
 
+
+$("a").on("click", function(e){
+  e.preventDefault();
+  var href = $(this).attr('href');
+  href = href.substr(1);
+  router.navigate(href, {trigger:true});
+});
 
 
 
