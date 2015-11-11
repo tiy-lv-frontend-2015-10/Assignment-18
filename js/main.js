1b028e0ffd69076be26e9aa46f40c89d3c927285
notewrangler <noiseartist@gmail.com>
2015-11-11 12:52:13 -0800

    added a route to a single model, returned it to the console and the page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-$.ajaxSetup({
-  headers: {
-    'X-Parse-Application-Id': 'VdpUcRAyrVKSMB1gkt5aNFEQ2HthRSFlEvWuDy6S',
-    'X-Parse-REST-API-Key': 'evvpQDmb7CFPSUlinNRqYre2Z7scwVrwzA7Nlijf'
-  }
-});
 
 var Senator = Backbone.Model.extend({
   initialize: function(){
@@ -27,25 +21,26 @@ var Senator = Backbone.Model.extend({
       return "Party is Required";
     }
   },
-  _parse_class_name: "Senator"
+  _parse_class_name: "Senator",
+  idAttribute: "objectId"
 });
 
-var cSchumer = new Senator();
-cSchumer.set({
-  name: "Charles Schumer",
-  state: "NY",
-  party: "D",
-  yearElected: 1998
-});
-
-cSchumer.save({}, {
-        success: function(resp){
-          console.log("Senator successfully saved");
-        },
-        error: function(err) {
-          console.log("error: ", err);
-        }
-      });
+// var cSchumer = new Senator();
+// cSchumer.set({
+//   name: "Charles Schumer",
+//   state: "NY",
+//   party: "D",
+//   yearElected: 1998
+// });
+//
+// cSchumer.save({}, {
+//         success: function(resp){
+//           console.log("Senator successfully saved");
+//         },
+//         error: function(err) {
+//           console.log("error: ", err);
+//         }
+//       });
 
 
 var Senators = Backbone.Collection.extend({
@@ -72,71 +67,46 @@ theSenate.fetch({
       }
     });
 
+    var Router = Backbone.Router.extend({
+      initialize: function () {
+        Backbone.history.start({pushState: true});
+      },
+      routes: {
+        "member/:objectID": "member",
+        "": "index"
+      }
+    });
+
+    var router = new Router();
+
+    router.on('route:member', function(objectId) {
+      var joe = new Senator({objectId: objectId});
+      joe.fetch({
+        success: function(resp){
+          console.log(resp);
+          var joeResponse = {
+            fact: resp
+           };
+          var joeTemplate = $('#joe').text();
+          var joeHTML = Mustache.render(joeTemplate, joeResponse);
+          $('#here-is-joe').html(joeHTML);
+        },
+        error: function(err){
+          console.log(err);
+        }
+      });
+
+    });
+
+    router.on('route:index', function () {
+      console.log('home page');
+    });
 
 
 
-
-
-
-// var Song = Backbone.Model.extend({
-//   initialize: function () {
-//     console.log("A new song has been created");
-//   },
-//   defaults: {
-//     title: null,
-//     artist: null,
-//     isCool: true,
-//     publishYear: null
-//   },
-//   validate: function (attrs) {
-//     if (!attrs.title) {
-//       return "Title is required";
-//     }
-//     if (!attrs.artist) {
-//       return "Artist is required";
-//     }
-//     if (typeof attrs.title !== 'string') {
-//       return "Title must a string";
-//     }
-//     if (typeof attrs.artist !== 'string') {
-//       return "Artist must a string";
-//     }
-//     if (!attrs.publishYear) {
-//       return "Published Year is required";
-//     }
-//     if (isNaN(Number(attrs.publishYear))) {
-//       return "Published Year should be a number";
-//     }
-//   },
-//   _parse_class_name: "Song"
-// });
-//
-// var song = new Song({
-//   title: "Für Elise",
-//   artist: "Beethoven",
-//   publishYear: 1810
-// });
-//
-// var Songs = Backbone.Collection.extend({
-//   model: Song,
-//   _parse_class_name: "Song"
-// });
-//
-// var SongsCollection = new Songs();
-//
-// song.save(null, {
-//   success: function(resp) {
-//     console.log(resp);
-//
-//     SongsCollection.fetch({
-//       success: function(resp) {
-//         console.log("success: ", resp);
-//       }, error: function (err) {
-//         console.log("error: ", err);
-//       }
-//     });
-//   },
-//   error: function (err) {
-//     console.log(err);
-//   }
-// });
+    $("a").on('click', function(e){
+      e.preventDefault();
+      var href = $(this).attr('href');
+      href = href.substr(1);
+      router.navigate(href, {trigger:true});
+    });
